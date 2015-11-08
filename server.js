11259d772e368e4347c14c8a76ef06017127b3b2
Amanda Keogh <amanda.keogh@gmail.com>
2015-11-07 23:24:05 -0800

    add v1 writeDirectory function, tests

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+var http = require('http');
+var Router = require(__dirname + '/lib/router.js');
+var writeDirectory = require(__dirname + '/lib/writeDirectory.js');
+
+var router = new Router();
+
+router.get('/test', function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Test worked again!');
+  res.end();
+});
+
+var server = http.createServer(function(req, res) {
+  router.route(req, res);
+}).listen(3000, function() {
+  console.log('server listening on port 3000');
+});
